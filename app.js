bdb782f720949e4f224aff0e0619b9647cceb4e7
Henry Huntington <henryxhuntington@gmail.com>
2022-11-02 17:28:16 -0700

    grabbed dom elements

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-/* Imports */
-
 /* Get DOM Elements */
+const temperatureSelect = document.getElementById('temperature-select');
+const architectureSelect = document.getElementById('architecture-select');
+const cuisineSelect = document.getElementById('cuisine-select');
 
+const tempImg = document.getElementById('temp-img');
+const archImg = document.getElementById('arch-img');
+const foodImg = document.getElementById('food-img');
 /* State */
 
 /* Events */
